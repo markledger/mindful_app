67e0908b5b9ad15f4b9f6840301c407446ebefdd
Mark Ledger <mark_ledger@outlook.com>
2024-11-20 12:14:47 +0000

    Shadow on welcome text

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class IntroScreen extends StatelessWidget{
+class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/Sea.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Align(
+              alignment: Alignment(0, -0.5),
+              child: Text(
+                "Welcome",
+                style: TextStyle(
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 5,
+                      color: Colors.black,
+                      offset: Offset(0, 0),
+                    )
+                  ],
+                  fontSize: 64,
+                ),
+              )),
+          Align(
+              alignment: const Alignment(0, 0.5),
+              child: ElevatedButton(onPressed: () {}, child: Text('Start'))),
+        ],
+      ),
+    );
   }
 }
